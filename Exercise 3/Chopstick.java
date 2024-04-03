a0c4add7a43a4279d5a40ebacf464964bd52d842
Nick Mascioli <namsc013@uottawa.ca>
2024-04-03 16:48:50 -0400

    Finished part 3 and 4

---
+++ Exercise 3/Chopstick.java
@@ -0,0 +1,35 @@
+public class Chopstick {
+	private int ID;
+	private boolean free;
+
+	Chopstick(int ID) {
+		this.ID = ID;
+		this.free = true;
+	}
+	
+	synchronized boolean Take(int waitTime) {
+        long startTime = System.currentTimeMillis();
+        while (!free) {
+            try {
+                wait(waitTime);
+                if (System.currentTimeMillis() - startTime >= waitTime) {
+                    // Timeout reached, unable to get the chopstick
+                    return false;
+                }
+            } catch (InterruptedException e) {
+                System.out.print(e);
+            }
+        }
+        free = false;
+        return true;
+    }
+	
+	synchronized void release() {
+		free = true;
+		notify();
+	}
+	    
+	public int getID() {
+	    return(ID);
+	}
+}
